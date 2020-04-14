713625e31431d63e0e016adff42f4e7e8be6c696
Akbar <56234939+infomagnum159@users.noreply.github.com>
2020-04-15 01:27:09 +0600

    Первые изменения
    Добавили массивы

--- index.php
+++ index.php
@@ -3,39 +3,103 @@
 
 <?php 
 
-	define("DBNAME", "kinomonster");
-
-	$nickname = "Sergey";
-
-	$balance = 0;
-	$addBalance1 = 10;
-	$addBalance2 = 35;
-
-	$balance = $addBalance1 + $addBalance2;
-
-
-
-	$svetofor = "green";
-	/*if($svetofor == "green") {
-		echo "Светофор зеленый, я перехожу дорогу";
-	} else if($svetofor == "yellow") {
-		echo "Сейчас желтый Светофор";
-	} else {
-		echo "Жду когда загорится зеленый";
-	}
+/*$i = 1;*/
+/*
+while($i <= 10) {
+	echo $i."<br>";
+	$i++;
+}
 */
-	switch ($svetofor) {
-		case 'green':
-			echo "Светофор зеленый, я перехожу дорогу";
-			break;
-		case 'yellow':
-			echo "Сейчас желтый Светофор";
-			break;
-		
-		default:
-			echo "Жду когда загорится зеленый";
-			break;
+/*do {
+	echo $i."<br>";
+	$i++;
+} while($i <= 10);
+
+*/
+/*for($i = 0; $i <= 10; $i++) {
+	echo $i."<br>";
+}*/
+
+/*$arr[0] = "Продукты";
+$arr[1] = "Bottle";*/
+
+/*for ($i = 0; $i < count($arr); $i++) { 
+	echo $arr[$i]."<br>";
+}*/
+
+/*$arr = array("на титул Грэмми" => "Веном", "на титул топ из 5" => "Дэдпул");*/
+/*$arr = ["Безумный Макс", "Инстерстеллар", "Джентельмены","Росомаха"];*/
+/*$arr = ["на титул Грэмми" =>"Рэкетир", "на титул топ из 5" => "Районы"];*/
+$arr = ["продукты", "бутылка с водой"];
+/*unset($arr[0]);*/
+array_push($arr, "яблоко");
+array_unshift($arr, "апельсин");
+array_push($arr, "апельсин");
+$arr = array_unique($arr);
+/*$arr = array_reverse($arr);*/
+/*sort($arr);*/
+/*shuffle($arr);*/
+$arr2 = array("Кокос", "арбуз");
+$newarr = array_merge($arr, $arr2);
+
+foreach ($newarr as $key => $value) {
+	echo "Фильм  ".$key. " - Занимает ".$value."<br>";
+}
+
+/*$str = implode(", /", $newarr);
+echo "<br>".$str;*/
+$str = "продукты, бутылка с водой, Кокос";
+$strtoarr = explode(",",$str);
+
+echo "<pre>";
+print_r($strtoarr);
+echo "</pre>";
+
+/*$camera["market1"]["row1"][0] = "продукты";
+$camera["market1"]["row1"][1] = "штырь";
+$camera["market2"]["row2"][0] = "аспирин";
+$camera["market2"]["row2"][1] = "амфитамины";
+$camera["market2"]["row2"][2] = "андроид";*/
+
+$camera = array(
+	"kera1"=>array(
+		"row1"=>array(
+			"продукты", "сумка"
+		),
+		"row2"=>array(
+			"orange","lamp", "rabbit"
+		)
+	),
+	"kera2"=>array(
+		"man1"=>array(
+			"продукты", "сумка"
+		),
+		"row2"=>array(
+			"orange","lamp", "rabbit"
+		)
+	),
+	"kera7"=>array(
+		"man2"=>array(
+			"продукты", "сумка"
+		),
+		"row2"=>array(
+			"orange","lamp", "rabbit"
+		)
+	)
+);
+
+echo "<pre>";
+print_r($camera);
+echo "</pre>";
+
+foreach ($camera as $key => $value) {
+	foreach ($value as $key2 => $value2) {
+		foreach ($value2 as $key3 => $value3) {
+			echo $value3;
+		}
 	}
+}
+
 
 ?>
 
@@ -50,18 +114,6 @@
 </head>
 <body>
 	
-
-	<div><!--Привет, --><?php //echo $nickname; ?></div>
-
-	<?php
-		/*echo "<div>".$balance."</div>";*/
-		/*echo DBNAME;*/
-
-	?>
-
-	
-
-
 
 </body>
 </html>
